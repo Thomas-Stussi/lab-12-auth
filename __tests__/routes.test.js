2436c4b3884d98a232c89d69958705bb2a1b98c9
Thomas Stussi <tstussi06@gmail.com>
2020-08-18 15:15:40 -0700

    first test passed

---
+++ __tests__/routes.test.js
@@ -0,0 +1,74 @@
+require('dotenv').config();
+const { execSync } = require('child_process');
+const fakeRequest = require('supertest');
+const app = require('../lib/app');
+const client = require('../lib/client');
+describe('routes', () => {
+  let token;
+  const newTodo = {
+    id: 4,
+    todo: 'Testing!',
+    completed: false,
+    owner_id: 2,
+  };
+  beforeAll(async done => {
+    execSync('npm run setup-db');
+    client.connect();
+    const signInData = await fakeRequest(app)
+      .post('/auth/signup')
+      .send({
+        email: 'jon@user.com',
+        password: '1234'
+      });
+    token = signInData.body.token;
+    return done();
+  });
+  afterAll(done => {
+    return client.end(done);
+  });
+  test('returns a new todo when creating new todo', async(done) => {
+    const data = await fakeRequest(app)
+      .post('/api/todos')
+      .send(newTodo)
+      .set('Authorization', token)
+      .expect('Content-Type', /json/)
+      .expect(200);
+    expect(data.body).toEqual(newTodo);
+    done();
+  });
+  
+  // test('returns all todos for the user when hitting GET /todos', async(done) => {
+  //   const expected = [
+  //     {
+  //       id: 4,
+  //       todo: 'Testing!',
+  //       completed: false,
+  //       owner_id: 2,
+  //     },
+  //   ];
+  //   const data = await fakeRequest(app)
+  //     .get('/api/todos')
+  //     .set('Authorization', token)
+  //     .expect('Content-Type', /json/)
+  //     .expect(200);
+  //   expect(data.body).toEqual(expected);
+  //   done();
+  // });
+
+  // test('returns a single todo for the user when hitting GET /todos/:id', async(done) => {
+  //   const expected = {
+  //     id: 4,
+  //     todo: 'Testing!',
+  //     completed: false,
+  //     owner_id: 2,
+  //   };
+  //   const data = await fakeRequest(app)
+  //     .get('/api/guitars/4')
+  //     .set('Authorization', token)
+  //     .expect('Content-Type', /json/)
+  //     .expect(200);
+  //   expect(data.body).toEqual(expected);
+  //   done();
+  // });
+  
+});
